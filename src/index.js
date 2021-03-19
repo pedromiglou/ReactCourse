764cfcb566fe73c2cfa2162bea681cbf7a7e4382
pedromiglou <50739814+pedromiglou@users.noreply.github.com>
2021-03-19 17:16:17 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+//args -> WHAT TO SHOW, WHERE TO SHOW IT
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
