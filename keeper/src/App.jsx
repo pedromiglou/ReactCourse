30d427a226a518ce3da81eff17328567c514a171
Pedro Amaral <pedromiglou@hotmail.com>
2021-03-22 22:12:09 +0000

    Keeper resolution

---
+++ keeper/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+
+function App() {
+    return (<div><Header></Header><Note></Note><Footer></Footer></div>);
+}
+
+export default App;
---
+++ keeper/src/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Footer() {
+    const year = new Date().getFullYear();
+    return <footer><p>copyright {year}</p></footer>
+}
+
+export default Footer;
