bffcbdd934736ab844bdddb96c4b47ae6284a440
Pedro Amaral <pedromiglou@hotmail.com>
2021-03-24 18:49:44 +0000

    Conditional Rendering, Ternary Operator & AND Operator

---
+++ 20 - Conditional Rendering, Ternary Operator & AND Operator/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./Login.jsx";
+
+var isLoggedIn = false;
+
+const currentTime = new Date().getHours();
+
+function App() {
+  return (
+    <div className="container">
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {currentTime > 12 && <h1>Why are you still working?</h1>}
+    </div>
+  );
+}
+
+export default App;
