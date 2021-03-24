a0c1d3d90d0324cfbd23e59bc5cec047cf0ae644
Pedro Amaral <pedromiglou@hotmail.com>
2021-03-24 17:37:30 +0000

    Keeper Project - added props and mapped data to components

---
+++ keeper/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+import notes from "./../notes.js";
+
+function App() {
+    return (<div>
+        <Header />
+        {notes.map(note => <Note key={note.key} title={note.title} content={note.content} />)}
+        <Footer />
+    </div>);
+}
+
+export default App;
